92899ae7543337b875c58896acb4fea856558b8a
Alec Smecher <alec@smecher.bc.ca>
2017-05-10 19:37:04 -0700

    Remove unnecessary constructors

--- controllers/grid/CustomBlockGridRow.inc.php
+++ controllers/grid/CustomBlockGridRow.inc.php
@@ -16,12 +16,6 @@
 import('lib.pkp.classes.controllers.grid.GridRow');
 
 class CustomBlockGridRow extends GridRow {
-	/**
-	 * Constructor
-	 */
-	function __construct() {
-		parent::__construct();
-	}
 
 	//
 	// Overridden template methods
