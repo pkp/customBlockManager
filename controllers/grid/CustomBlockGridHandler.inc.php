b978ffb579da087596ba19d14682ab6c31942471
Alec Smecher <alec@smecher.bc.ca>
2018-08-29 14:55:45 -0700

    pkp/pkp-lib#4024 Add ContextAccessPolicy

--- controllers/grid/CustomBlockGridHandler.inc.php
+++ controllers/grid/CustomBlockGridHandler.inc.php
@@ -37,6 +37,15 @@ class CustomBlockGridHandler extends GridHandler {
 	// Overridden template methods
 	//
 	/**
+	 * @copydoc PKPHandler::authorize()
+	 */
+	function authorize($request, &$args, $roleAssignments) {
+		import('lib.pkp.classes.security.authorization.ContextAccessPolicy');
+		$this->addPolicy(new ContextAccessPolicy($request, $roleAssignments));
+		return parent::authorize($request, $args, $roleAssignments);
+	}
+
+	/**
 	 * @copydoc GridHandler::initialize()
 	 */
 	function initialize($request, $args = null) {
