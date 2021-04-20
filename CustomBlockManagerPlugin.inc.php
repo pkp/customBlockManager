496fe4e7f15df6030f9505ec5eef3a7bfb56406e
Nate Wright <natew@notthisway.com>
2021-04-20 16:24:51 +0100

    pkp/pkp-lib#5678 Format all PHP files to PSR12

--- CustomBlockManagerPlugin.inc.php
+++ CustomBlockManagerPlugin.inc.php
@@ -16,134 +16,157 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
-class CustomBlockManagerPlugin extends GenericPlugin {
-	/**
-	 * @copydoc Plugin::getDisplayName()
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.customBlockManager.displayName');
-	}
+class CustomBlockManagerPlugin extends GenericPlugin
+{
+    /**
+     * @copydoc Plugin::getDisplayName()
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.generic.customBlockManager.displayName');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription()
-	 */
-	function getDescription() {
-		return __('plugins.generic.customBlockManager.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription()
+     */
+    public function getDescription()
+    {
+        return __('plugins.generic.customBlockManager.description');
+    }
 
-	/**
-	 * @copydoc Plugin::register()
-	 */
-	function register($category, $path, $mainContextId = null) {
-		if (parent::register($category, $path, $mainContextId)) {
-			// If the system isn't installed, or is performing an upgrade, don't
-			// register hooks. This will prevent DB access attempts before the
-			// schema is installed.
-			if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
+    /**
+     * @copydoc Plugin::register()
+     *
+     * @param null|mixed $mainContextId
+     */
+    public function register($category, $path, $mainContextId = null)
+    {
+        if (parent::register($category, $path, $mainContextId)) {
+            // If the system isn't installed, or is performing an upgrade, don't
+            // register hooks. This will prevent DB access attempts before the
+            // schema is installed.
+            if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) {
+                return true;
+            }
 
-			if ($this->getEnabled($mainContextId)) {
-				$this->import('CustomBlockPlugin');
+            if ($this->getEnabled($mainContextId)) {
+                $this->import('CustomBlockPlugin');
 
-				// Ensure that there is a context (journal or press)
-				if ($request = Application::get()->getRequest()) {
-					if ($mainContextId) {
-						$contextId = $mainContextId;
-					} else {
-						$context = $request->getContext();
-						$contextId = $context ? $context->getId() : CONTEXT_SITE;
-					}
+                // Ensure that there is a context (journal or press)
+                if ($request = Application::get()->getRequest()) {
+                    if ($mainContextId) {
+                        $contextId = $mainContextId;
+                    } else {
+                        $context = $request->getContext();
+                        $contextId = $context ? $context->getId() : CONTEXT_SITE;
+                    }
 
-					// Load the custom blocks we have created
-					$blocks = $this->getSetting($contextId, 'blocks');
-					if (!is_array($blocks)) $blocks = array();
+                    // Load the custom blocks we have created
+                    $blocks = $this->getSetting($contextId, 'blocks');
+                    if (!is_array($blocks)) {
+                        $blocks = [];
+                    }
 
-					// Loop through each custom block and register it
-					$i=0;
-					foreach ($blocks as $block) {
-						PluginRegistry::register(
-							'blocks',
-							new CustomBlockPlugin($block, $this),
-							$this->getPluginPath()
-						);
-					}
-				}
+                    // Loop through each custom block and register it
+                    $i = 0;
+                    foreach ($blocks as $block) {
+                        PluginRegistry::register(
+                            'blocks',
+                            new CustomBlockPlugin($block, $this),
+                            $this->getPluginPath()
+                        );
+                    }
+                }
 
-				// This hook is used to register the components this plugin implements to
-				// permit administration of custom block plugins.
-				HookRegistry::register('LoadComponentHandler', array($this, 'setupGridHandler'));
-			}
-			return true;
-		}
-		return false;
-	}
+                // This hook is used to register the components this plugin implements to
+                // permit administration of custom block plugins.
+                HookRegistry::register('LoadComponentHandler', [$this, 'setupGridHandler']);
+            }
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Permit requests to the custom block grid handler
-	 * @param $hookName string The name of the hook being invoked
-	 * @param $args array The parameters to the invoked hook
-	 */
-	function setupGridHandler($hookName, $params) {
-		$component =& $params[0];
-		if ($component == 'plugins.generic.customBlockManager.controllers.grid.CustomBlockGridHandler') {
-			define('CUSTOMBLOCKMANAGER_PLUGIN_NAME', $this->getName());
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Permit requests to the custom block grid handler
+     *
+     * @param $hookName string The name of the hook being invoked
+     */
+    public function setupGridHandler($hookName, $params)
+    {
+        $component = & $params[0];
+        if ($component == 'plugins.generic.customBlockManager.controllers.grid.CustomBlockGridHandler') {
+            define('CUSTOMBLOCKMANAGER_PLUGIN_NAME', $this->getName());
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * @copydoc Plugin::getActions()
-	 */
-	function getActions($request, $actionArgs) {
-		import('lib.pkp.classes.linkAction.request.AjaxModal');
-		$router = $request->getRouter();
-		return array_merge(
-			$this->getEnabled()?array(
-				new LinkAction(
-					'settings',
-					new AjaxModal(
-						$router->url(
-							$request, null, null, 'manage', null, array(
-								'plugin' => $this->getName(),
-								'category' => $this->getCategory(),
-								'action' => 'index'
-							)
-						),
-						$this->getDisplayName()
-					),
-					__('plugins.generic.customBlockManager.manage'),
-					null
-				)
-			):array(),
-			parent::getActions($request, $actionArgs)
-		);
-	}
+    /**
+     * @copydoc Plugin::getActions()
+     */
+    public function getActions($request, $actionArgs)
+    {
+        import('lib.pkp.classes.linkAction.request.AjaxModal');
+        $router = $request->getRouter();
+        return array_merge(
+            $this->getEnabled() ? [
+                new LinkAction(
+                    'settings',
+                    new AjaxModal(
+                        $router->url(
+                            $request,
+                            null,
+                            null,
+                            'manage',
+                            null,
+                            [
+                                'plugin' => $this->getName(),
+                                'category' => $this->getCategory(),
+                                'action' => 'index'
+                            ]
+                        ),
+                        $this->getDisplayName()
+                    ),
+                    __('plugins.generic.customBlockManager.manage'),
+                    null
+                )
+            ] : [],
+            parent::getActions($request, $actionArgs)
+        );
+    }
 
-	/**
-	 * @copydoc Plugin::manage()
-	 */
-	function manage($args, $request) {
-		$templateMgr = TemplateManager::getManager($request);
-		$dispatcher = $request->getDispatcher();
-		return $templateMgr->fetchAjax(
-			'customBlockGridUrlGridContainer',
-			$dispatcher->url(
-				$request, PKPApplication::ROUTE_COMPONENT, null,
-				'plugins.generic.customBlockManager.controllers.grid.CustomBlockGridHandler', 'fetchGrid'
-			)
-		);
-	}
+    /**
+     * @copydoc Plugin::manage()
+     */
+    public function manage($args, $request)
+    {
+        $templateMgr = TemplateManager::getManager($request);
+        $dispatcher = $request->getDispatcher();
+        return $templateMgr->fetchAjax(
+            'customBlockGridUrlGridContainer',
+            $dispatcher->url(
+                $request,
+                PKPApplication::ROUTE_COMPONENT,
+                null,
+                'plugins.generic.customBlockManager.controllers.grid.CustomBlockGridHandler',
+                'fetchGrid'
+            )
+        );
+    }
 
-	/**
-	 * This plugin can be used site-wide or in a specific context. The
-	 * isSitePlugin check is used to grant access to different users, so this
-	 * plugin must return true only if the user is currently in the site-wide
-	 * context.
-	 *
-	 * @see PluginGridRow::_canEdit()
-	 * @return boolean
-	 */
-	function isSitePlugin() {
-		return !Application::get()->getRequest()->getContext();
-	}
+    /**
+     * This plugin can be used site-wide or in a specific context. The
+     * isSitePlugin check is used to grant access to different users, so this
+     * plugin must return true only if the user is currently in the site-wide
+     * context.
+     *
+     * @see PluginGridRow::_canEdit()
+     *
+     * @return boolean
+     */
+    public function isSitePlugin()
+    {
+        return !Application::get()->getRequest()->getContext();
+    }
 }
--- controllers/grid/form/CustomBlockForm.inc.php
+++ controllers/grid/form/CustomBlockForm.inc.php
@@ -16,99 +16,106 @@
 
 import('lib.pkp.classes.form.Form');
 
-class CustomBlockForm extends Form {
-	/** @var int Context (press / journal) ID */
-	var $contextId;
+class CustomBlockForm extends Form
+{
+    /** @var int Context (press / journal) ID */
+    public $contextId;
 
-	/** @var CustomBlockPlugin Custom block plugin */
-	var $plugin;
+    /** @var CustomBlockPlugin Custom block plugin */
+    public $plugin;
 
-	/**
-	 * Constructor
-	 * @param $template string the path to the form template file
-	 * @param $contextId int
-	 * @param $plugin CustomBlockPlugin
-	 */
-	function __construct($template, $contextId, $plugin = null) {
-		parent::__construct($template);
+    /**
+     * Constructor
+     *
+     * @param $template string the path to the form template file
+     * @param $contextId int
+     * @param $plugin CustomBlockPlugin
+     */
+    public function __construct($template, $contextId, $plugin = null)
+    {
+        parent::__construct($template);
 
-		$this->contextId = $contextId;
-		$this->plugin = $plugin;
+        $this->contextId = $contextId;
+        $this->plugin = $plugin;
 
-		// Add form checks
-		$this->addCheck(new FormValidatorPost($this));
-		$this->addCheck(new FormValidatorCSRF($this));
-		$this->addCheck(new FormValidator($this, 'blockTitle', 'required', 'plugins.generic.customBlock.nameRequired'));
-	}
+        // Add form checks
+        $this->addCheck(new FormValidatorPost($this));
+        $this->addCheck(new FormValidatorCSRF($this));
+        $this->addCheck(new FormValidator($this, 'blockTitle', 'required', 'plugins.generic.customBlock.nameRequired'));
+    }
 
-	/**
-	 * Initialize form data from current group group.
-	 */
-	function initData() {
-		$contextId = $this->contextId;
-		$plugin = $this->plugin;
+    /**
+     * Initialize form data from current group group.
+     */
+    public function initData()
+    {
+        $contextId = $this->contextId;
+        $plugin = $this->plugin;
 
-		$templateMgr = TemplateManager::getManager();
+        $templateMgr = TemplateManager::getManager();
 
-		$existingBlockName = null;
-		$blockTitle = null;
-		$blockContent = null;
-		$showName = null;
-		if ($plugin) {
-			$blockTitle = $plugin->getSetting($contextId, 'blockTitle');
-			$blockContent = $plugin->getSetting($contextId, 'blockContent');
-			$showName = $plugin->getSetting($contextId, 'showName');
-			$existingBlockName = $plugin->_blockName;
-		}
-		$this->setData('blockContent', $blockContent);
-		$this->setData('blockTitle', $blockTitle);
-		$this->setData('showName', $showName);
-		$this->setData('existingBlockName', $existingBlockName);
-	}
+        $existingBlockName = null;
+        $blockTitle = null;
+        $blockContent = null;
+        $showName = null;
+        if ($plugin) {
+            $blockTitle = $plugin->getSetting($contextId, 'blockTitle');
+            $blockContent = $plugin->getSetting($contextId, 'blockContent');
+            $showName = $plugin->getSetting($contextId, 'showName');
+            $existingBlockName = $plugin->_blockName;
+        }
+        $this->setData('blockContent', $blockContent);
+        $this->setData('blockTitle', $blockTitle);
+        $this->setData('showName', $showName);
+        $this->setData('existingBlockName', $existingBlockName);
+    }
 
-	/**
-	 * Assign form data to user-submitted data.
-	 */
-	function readInputData() {
-		$this->readUserVars(array('blockTitle', 'blockContent', 'showName'));
-	}
+    /**
+     * Assign form data to user-submitted data.
+     */
+    public function readInputData()
+    {
+        $this->readUserVars(['blockTitle', 'blockContent', 'showName']);
+    }
 
-	/**
-	 * @copydoc Form::execute()
-	 */
-	function execute(...$functionArgs) {
-		$plugin = $this->plugin;
-		$contextId = $this->contextId;
-		if (!$plugin) {
-			$locale = AppLocale::getLocale();
+    /**
+     * @copydoc Form::execute()
+     */
+    public function execute(...$functionArgs)
+    {
+        $plugin = $this->plugin;
+        $contextId = $this->contextId;
+        if (!$plugin) {
+            $locale = AppLocale::getLocale();
 
-			// Add the custom block to the list of the custom block plugins in the
-			// custom block manager plugin
-			$customBlockManagerPlugin = PluginRegistry::getPlugin('generic', CUSTOMBLOCKMANAGER_PLUGIN_NAME);
-			$blocks = $customBlockManagerPlugin->getSetting($contextId, 'blocks');
-			if (!isset($blocks)) $blocks = [];
+            // Add the custom block to the list of the custom block plugins in the
+            // custom block manager plugin
+            $customBlockManagerPlugin = PluginRegistry::getPlugin('generic', CUSTOMBLOCKMANAGER_PLUGIN_NAME);
+            $blocks = $customBlockManagerPlugin->getSetting($contextId, 'blocks');
+            if (!isset($blocks)) {
+                $blocks = [];
+            }
 
 
-			$blockName = \Stringy\Stringy::create($this->getData('blockTitle')[$locale])->toLowerCase()->dasherize()->regexReplace('[^a-z0-9\-\_.]', '');
-			if (in_array($blockName, $blocks)) {
-				$blockName = uniqid($blockName);
-			}
-			$blocks[] = (string) $blockName;
-			$customBlockManagerPlugin->updateSetting($contextId, 'blocks', $blocks);
+            $blockName = \Stringy\Stringy::create($this->getData('blockTitle')[$locale])->toLowerCase()->dasherize()->regexReplace('[^a-z0-9\-\_.]', '');
+            if (in_array($blockName, $blocks)) {
+                $blockName = uniqid($blockName);
+            }
+            $blocks[] = (string) $blockName;
+            $customBlockManagerPlugin->updateSetting($contextId, 'blocks', $blocks);
 
-			// Create a new custom block plugin
-			import('plugins.generic.customBlockManager.CustomBlockPlugin');
-			$plugin = new CustomBlockPlugin($blockName, $customBlockManagerPlugin);
-			// Default the block to being enabled
-			$plugin->setEnabled(true);
-		}
+            // Create a new custom block plugin
+            import('plugins.generic.customBlockManager.CustomBlockPlugin');
+            $plugin = new CustomBlockPlugin($blockName, $customBlockManagerPlugin);
+            // Default the block to being enabled
+            $plugin->setEnabled(true);
+        }
 
-		// update custom block plugin content
-		$plugin->updateSetting($contextId, 'blockTitle', $this->getData('blockTitle'));
-		$plugin->updateSetting($contextId, 'blockContent', $this->getData('blockContent'));
-		$plugin->updateSetting($contextId, 'showName', $this->getData('showName'));
+        // update custom block plugin content
+        $plugin->updateSetting($contextId, 'blockTitle', $this->getData('blockTitle'));
+        $plugin->updateSetting($contextId, 'blockContent', $this->getData('blockContent'));
+        $plugin->updateSetting($contextId, 'showName', $this->getData('showName'));
 
-		parent::execute(...$functionArgs);
-	}
+        parent::execute(...$functionArgs);
+    }
 }
-
